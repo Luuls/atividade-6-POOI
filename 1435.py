89b2626db5821ffbb8f775abda36a6350541cdcd
Luan Moraes <78831999+Luuls@users.noreply.github.com>
2023-05-04 15:52:16 -0300

    Add files via upload

--- 1435.py
+++ 1435.py
@@ -1,2 +1,24 @@
+# https://www.beecrowd.com.br/judge/pt/problems/view/1435
+
 while (n := int(input())) != 0:
-    # matrix = 
+    if n == 1:
+        print('  1\n')
+        continue
+
+    matrix = [[None for _ in range(n)] for _ in range(n)]
+    for layer in range(n // 2 + 1):
+        for i in range(layer, layer + n - 2 * layer):
+            matrix[layer][i] = layer + 1
+            matrix[-layer - 1][i] = layer + 1
+
+            matrix[i][layer] = layer + 1
+            matrix[i][-layer - 1] = layer + 1
+
+    for i in range(n):
+        print('  ', end='')
+        for j in range(n - 1):
+            print(f'{matrix[i][j]:<4}', end='')
+
+        print(matrix[i][-1])
+
+    print()
